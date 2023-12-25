35f66d17e9ec4f850cf430cebd7fe63abe43421d
NxPKG <116948796+NxPKG@users.noreply.github.com>
2023-12-25 07:45:19 +0600

    Update validate_config.py

--- examples/validate_config.py
+++ examples/validate_config.py
@@ -1,10 +1,14 @@
 import sys
+import os
 from configparser import ConfigParser
+
+# Assuming validate_config.py is in the examples directory
+config_path = os.path.join(os.path.dirname(__file__), 'config.ini')
 
 config = ConfigParser()
 
 try:
-    config.read('config.ini')
+    config.read(config_path)
 
     # Check for the existence of the 'keys' section
     if 'keys' not in config:
@@ -12,7 +16,13 @@ try:
 
     # Check for the presence of 'api_key' and 'api_secret' keys
     if 'api_key' not in config['keys'] or 'api_secret' not in config['keys']:
-        raise ValueError("Missing 'api_key' or 'api_secret' in the 'keys' section")
+        raise ValueError("Both 'api_key' and 'api_secret' are required in the 'keys' section")
+
+    # Access the API_KEY and API_SECRET from config.ini
+    api_key = config['keys']['api_key']
+    api_secret = config['keys']['api_secret']
+
+    # ... Your additional validation logic here ...
 
     print("Config validation successful")
 
