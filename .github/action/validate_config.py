6afcea247c62f392f0f9b2c37169cc83c29c3d9c
NxPKG <116948796+NxPKG@users.noreply.github.com>
2023-12-25 08:03:01 +0600

    Update validate_config.py

--- .github/action/validate_config.py
+++ .github/action/validate_config.py
@@ -1,24 +1,20 @@
 import sys
-import os
 from configparser import ConfigParser
-
-# Assuming validate_config.py is in the examples directory
-config_path = os.path.join(os.path.dirname(__file__), 'config.ini')
 
 config = ConfigParser()
 
 try:
-    config.read(config_path)
+    config.read('examples/config.ini')
 
     # Check for the existence of the 'keys' section
     if 'keys' not in config:
-        raise ValueError("Missing 'keys' section in config.ini")
+        raise ValueError("Missing 'keys' section in examples/config.ini")
 
     # Check for the presence of 'api_key' and 'api_secret' keys
     if 'api_key' not in config['keys'] or 'api_secret' not in config['keys']:
         raise ValueError("Both 'api_key' and 'api_secret' are required in the 'keys' section")
 
-    # Access the API_KEY and API_SECRET from config.ini
+    # Access the API_KEY and API_SECRET from examples/config.ini
     api_key = config['keys']['api_key']
     api_secret = config['keys']['api_secret']
 
